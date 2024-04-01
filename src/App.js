3bf080eb77c9d4a70fe3cbb7a73a8059ad3247b6
Kapil Chaudhary <kc685838@gmail.com>
2024-04-01 23:36:44 +0530

    Added: Tailwind Integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import './styles/tailwind.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-4xl">
       Let's start to create my portfolio🥰.
     </div>
   );
